c0c976270e57e6ab6cf227b3df7ae25f465380aa
Danny Mussie <dannymussie7@gmail.com>
2021-11-04 23:32:15 -0500

    implemented MUI button in home page to naviage to add movie page

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import MovieItem from '../MovieItem/MovieItem';
+import Button from '@material-ui/core/Button';
 import './MovieList.css'
 
 function MovieList() {
@@ -10,16 +12,27 @@ function MovieList() {
     // the retrieval of data from our redux store
     const movies = useSelector(store => store.movies);
     
+    const history = useHistory();
+    
     // this useEffect function will enable the dispatch to occur on page load
     useEffect(() => {
         dispatch({ type: 'FETCH_MOVIES' }); 
     }, []);
 
+    const handleToAddMovie = () => {
+        history.push('/addmovie')
+    }
+
     // in this return, we render the mapping of our movie data from the redux store
     // this data is then appended to the DOM once its data is passed down via props in the MovieItem component
     return (
         <main>
-            <h1>MovieList</h1>
+            <Button 
+            variant="contained" 
+            color="primary" 
+            onClick={handleToAddMovie}>
+                Add a Movie!
+            </Button>
             <section className="movies">
                 {movies.map(movie => {
                     return (
